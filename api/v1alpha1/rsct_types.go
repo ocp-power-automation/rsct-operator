200ce291928d37970b38741b2f56fe3e059ee3bf
manju956 <manjunath.a.c@ibm.com>
2024-09-27 13:26:05 +0530

    RSCT operator status changes
    Signed-off-by: manju956 <manjunath.a.c@ibm.com>

--- api/v1alpha1/rsct_types.go
+++ api/v1alpha1/rsct_types.go
@@ -38,6 +38,7 @@ type RSCTSpec struct {
 type RSCTStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	State *string `json:"state,omitempty"`
 }
 
 //+kubebuilder:object:root=true
