9aad6043ae749d34c8e8bc74e5b4ce0c19e0457a
Sathvik S <Sathvik.S@ibm.com>
2026-04-08 10:26:46 +0530

    Validation webhook to enforce single RSCT instance across all NS (#171)
    Signed-off-by: Sathvik <Sathvik.S@ibm.com>

--- cmd/main.go
+++ cmd/main.go
@@ -150,6 +150,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "RSCT")
 		os.Exit(1)
 	}
+	if err = (&rsctv1alpha1.RSCT{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "RSCT")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
